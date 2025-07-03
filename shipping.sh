3826fc17719eb7fdc6957b589c41ae85ebe312fc
akashabalaji <akashabalaji123@gmail.com>
2025-07-03 16:35:11 +0530

    adding config files

---
+++ shipping.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+START_TIME=@(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/logs/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
+
+#check the user has root previleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE # Exit if not root
+    exit 1 # Exit with error code 1
+    else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+echo "Please enter root password to setup"
+read -s MYSQL_ROOT_PASSWORD
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+dnf install maven -y &>>$LOG_FILE
+VALIDATE $? "Installing Maven & Java"
+
+id roboshop &>>$LOG_FILE
+# Check if the user exists, if not create it
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+else
+    echo" Roboshop user already exists..$Y Skipping $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating /app directory"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading shipping.zip"
+
+rm -rf /app/*
+cd /app
+unzip /tmp/shipping.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping shipping.zip"
+
+mvn clean package &>>$LOG_FILE
+VALIDATE $? "Packaging shipping application"
+
+mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE    
+VALIDATE $? "Renaming shipping jar file"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOG_FILE
+
+systemctl daemon-relaod &>>$LOG_FILE
+VALIDATE $? "Reloading systemd daemon"
+
+systemctl enable shipping &>>$LOG_FILE
+VALIDATE $? "Enabling shipping service"
+
+systemctl start shipping &>>$LOG_FILE
+VALIDATE $? "Starting shipping service"
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL client"
+
+mysql -h mysql.akashbalaji.site -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+if [ $? -ne 0  ]
+then 
+    mysql -h mysql.akashabalaji.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.akashabalaji.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h mysql.akashabalaji.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    VALIDATE $? "Loading data into MySQL"
+else
+    echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
+fi
+
+systemctl  restart shipping &>>$LOG_FILE
+VALIDATE $? "Restarting shipping service"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
